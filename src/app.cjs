bf3d72f6c00771345416e630e4577ad182aa1f95
WickyPlays <nguyenthienbao8601@gmail.com>
2024-12-13 16:28:19 +0700

    Update

--- src/app.cjs
+++ src/app.cjs
@@ -14,22 +14,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-let GOOGLE_KEY = `-----BEGIN PRIVATE KEY-----\n${process.env.PRIVATE_KEY}\n-----END PRIVATE KEY-----\n`;
-
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: GOOGLE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-  },
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  keyFile: path.join(__dirname, "../.data/service_account.json"),
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
 
 const sheets = google.sheets({ version: "v4", auth });
